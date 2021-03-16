7e0d9949d18a1e351f297581bbd706300f02ec57
802ScubaFish <christianrdow@gmail.com>
2021-03-16 16:01:35 -0400

    setting up files and refs for students

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
 const publicPath = path.resolve('./public');
+// Heroku = 
+// npm run build - to create build folder 
+// Update line 5 to point to new build'./public/build'
+// Follow heroku start up guide for new app here - https://devcenter.heroku.com/articles/git
+// git push heroku master - to push to heroku..let run its build sequence, check heroku live app
 
 app.use(express.static(publicPath));
 
